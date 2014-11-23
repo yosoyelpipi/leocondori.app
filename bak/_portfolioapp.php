c38c49e11a8c347fa1507ad2a5c42e0e6eaad313
yosoyelpipi <lcondori@gmail.com>
2014-11-23 00:29:06 -0300

    Cambios en contacto y portfolio
    Vamos para la publicación

---
+++ bak/_portfolioapp.php
@@ -0,0 +1,3 @@
+<?php
+echo "Proximamente mi portfolio...";
+?>
---
+++ bak/contactoapp.php
@@ -0,0 +1,8 @@
+<?php
+$email = $_POST["email"];
+$comentarios = $_POST["comentarios"];
+
+echo "recibido, email: ".$email;
+echo '<br>';
+echo "Tus comentarios: ".$comentarios;
+?>
